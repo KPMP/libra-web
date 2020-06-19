3290bf22e80e0f176f360fe7d7dda2ff02cae5f8
Becky Reamy <rlreamy@umich.edu>
2020-06-19 14:40:20 -0400

    KPMP-1917: Finishing touches

--- src/components/Validation/FilenameValidationPage.js
+++ src/components/Validation/FilenameValidationPage.js
@@ -32,23 +32,25 @@ class FilenameValidationPage extends Component {
 			<Container>
 				<form onSubmit={this.handleSubmit}>
 					<Row className='mt-3'>
-						<Col sm={4}>
+						<Col sm={3}>
 							<label>Package Id:</label>
 						</Col>
-						<Col sm={8}>
-							<input type='text' ref={(input) => this.packageId = input} onChange={this.handlePackageIdChange}/>
+						<Col sm={9}>
+							<input type='text' size={40} ref={(input) => this.packageId = input} onChange={this.handlePackageIdChange}/>
 						</Col>
 					</Row>
 					<Row className='mt-3'>
-						<Col sm={4}>
+						<Col sm={3}>
 							<label>Files in metadata spreadsheet:</label>
 						</Col>
-						<Col sm={8}>
+						<Col sm={9}>
 							<textarea name='filenames' id='filenames' rows='10' cols='100' onChange={this.handleFilenamesChange} ref={(input) => this.filenames = input}/>
 						</Col>
 					</Row>
 					<Row className='mt-3'>
-						<Col sm={12} className='float-right'>
+						<Col sm={3} >
+						</Col>
+						<Col sm={9}>
 							<Button color='primary' type="submit" disabled={!isEnabled}>Validate</Button>
 						</Col>
 					</Row>
