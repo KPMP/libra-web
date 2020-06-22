ad8ecd96eea5ce0de9e41d4d12882708e556569d
Becky Reamy <rlreamy@umich.edu>
2020-06-22 08:58:04 -0400

    KPMP-1917: Fixing some codacy issues

--- src/components/PackageDashboard/PackageDashboardPage.js
+++ src/components/PackageDashboard/PackageDashboardPage.js
@@ -5,28 +5,30 @@ import PackageTable from './PackageTable';
 
 class PackageDashboardPage extends Component {
 
-    componentDidMount() {
-        this.props.getPackages();
-    }
+	componentDidMount() {
+		this.props.getPackages();
+	}
 
-    render() {
-        return <Container id='package-dashboard-page'>
-        	<Row className='mt-3'>
-        		<Col sm={12}>
-        			<Link className='float-right' to='/filenameValidation'>
-        				<Button color="primary">Validate Filenames</Button>
-        			</Link>
-        		</Col>
-        	</Row>
-            <Row className='mt-3'>
-                <Col sm={12}>
-                    <PackageTable
-                        packages={this.props.packages}
-                    />
-                </Col>
-            </Row>
-        </Container>;
-    }
+	render() {
+		return ( 
+			<Container id='package-dashboard-page'>
+				<Row className='mt-3'>
+					<Col sm={12}>
+						<Link className='float-right' to='/filenameValidation'>
+							<Button color="primary">Validate Filenames</Button>
+						</Link>
+					</Col>
+				</Row>
+				<Row className='mt-3'>
+					<Col sm={12}>
+					<PackageTable
+					packages={this.props.packages}
+					/>
+					</Col>
+				</Row>
+			</Container>;
+		);
+	}
 }
 
 export default PackageDashboardPage;
