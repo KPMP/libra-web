de19f3c1a9abf5acaaa5e6f13efb98d39194be46
Becky Reamy <rlreamy@umich.edu>
2020-08-24 14:59:00 -0400

    KPMP-2048: Added tests

--- src/components/PackageDashboard/stateDisplayHelper.js
+++ src/components/PackageDashboard/stateDisplayHelper.js
@@ -1,14 +1,14 @@
 export const getStateDisplayText = (state, stateDisplayMap) => {
-	console.log(state);
-	console.log(stateDisplayMap);
 	
 	let stateDisplayText = stateDisplayMap.filter(function(stateDisplayItem) {
 		if (stateDisplayItem.state === state.state) {
 			return stateDisplayItem;
-		} else {
-			return '';
-		}
+		} 
 	}, state);
 	
-	return stateDisplayText[0].apps.dmd.text
+	if (stateDisplayText[0]) {
+		return stateDisplayText[0].apps.dmd.text
+	} else {
+		return '';
+	}
 }
