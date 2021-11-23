7ac668aba2fa67af11515b73b887694c79dc4076
Becky Reamy <rlreamy@umich.edu>
2021-11-23 16:01:39 -0500

    KPMP-1161: Add styling and export

--- src/components/PackageDashboard/PackageTable.js
+++ src/components/PackageDashboard/PackageTable.js
@@ -3,8 +3,11 @@ import ReactTable from 'react-table';
 import ReactGA from 'react-ga';
 import PropTypes from 'prop-types';
 import Moment from 'moment';
-import { Button } from 'reactstrap';
+import { Row, Col, Button } from 'reactstrap';
 import { getStateDisplayText } from './stateDisplayHelper';
+import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { CSVLink } from 'react-csv';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const PACKAGE_ID_LABEL = 'Package ID';
 const PACKAGE_TYPE_LABEL = 'Package Type';
@@ -48,6 +51,7 @@ class PackageTable extends Component {
 			columns: this.getColumns()
 		};
 	};
+
 
 	getColumns() {
 		return [
@@ -168,22 +172,52 @@ class PackageTable extends Component {
 		});
 	}
 
+	prepareData = (packages) => {
+		return packages.map((pkg) => {
+			return {
+				[PACKAGE_ID_LABEL]: pkg[PACKAGE_INFO_PROPERTY][PACKAGE_ID],
+				[SUBJECT_ID_LABEL]: pkg[PACKAGE_INFO_PROPERTY][SUBJECT_ID],
+				[PACKAGE_TYPE_LABEL]: pkg[PACKAGE_INFO_PROPERTY][PACKAGE_TYPE_ID],
+				[SUBMITTER_LABEL]: pkg[PACKAGE_INFO_PROPERTY].submitter && pkg[PACKAGE_INFO_PROPERTY].submitter[SUBMITTER_ID] ? pkg[PACKAGE_INFO_PROPERTY].submitter[SUBMITTER_ID] : pkg[PACKAGE_INFO_PROPERTY].submitter[SUBMITTER_FIRST_NAME] + ' ' + pkg[PACKAGE_INFO_PROPERTY].submitter[SUBMITTER_LAST_NAME],
+				[TIS_NAME_LABEL]: pkg[PACKAGE_INFO_PROPERTY][TIS_NAME_ID],
+				[DATE_SUBMITTED_LABEL]: new Moment(pkg[PACKAGE_INFO_PROPERTY][DATE_SUBMITTED_ID]).local().format(DATE_FORMAT),
+				[PACKAGE_STATE_LABEL]: getStateDisplayText(pkg.state, this.props.stateDisplayMap)
+			}
+		});
+	}
+
 	render() {
-		return <ReactTable
-			data={this.props.packages}
-			ref={this.reactTable}
-			sorted={this.state.sorted}
-			filtered={this.state.filtered}
-			onSortedChange={this.onSortedChange}
-			onFilteredChange={this.onFilteredChange}
-			columns={this.state.columns}
-			defaultPageSize={12}
-			defaultFilterMethod={this.defaultFilterMethod}
-			filterable
-			className='-striped -highlight'
-			showPageSizeOptions={false}
-			noDataText={'No packages found'}
-		/>;
+		return (
+			<article>
+			<Row><Col xs={12} className='mb-2'>
+				<CSVLink
+					data={this.prepareData(this.props.packages)}
+					filename={'dmd-package-info.csv'}
+					target="_blank"
+					className="text-body icon-container"
+				>
+					<FontAwesomeIcon icon={faDownload} pull='right' />
+				</CSVLink>
+			</Col></Row>
+			<Row><Col xs={12}>
+				<ReactTable
+					data={this.props.packages}
+					ref={this.reactTable}
+					sorted={this.state.sorted}
+					filtered={this.state.filtered}
+					onSortedChange={this.onSortedChange}
+					onFilteredChange={this.onFilteredChange}
+					columns={this.state.columns}
+					defaultPageSize={12}
+					defaultFilterMethod={this.defaultFilterMethod}
+					filterable
+					className='-striped -highlight'
+					showPageSizeOptions={false}
+					noDataText={'No packages found'}
+				/>
+			</Col></Row>
+		</article>
+		);
 	}
 }
 
