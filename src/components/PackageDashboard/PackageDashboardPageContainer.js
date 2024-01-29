7a2d96e163fbc25e9ccdff3cab3fecb841dfa4b6
Becky Reamy <rlreamy@umich.edu>
2024-01-29 15:21:46 -0500

    KPMP-4827: Check that data is loaded

--- src/components/PackageDashboard/PackageDashboardPageContainer.js
+++ src/components/PackageDashboard/PackageDashboardPageContainer.js
@@ -8,6 +8,9 @@ const mapStateToProps = (state, props) => ({
 });
 
 const mapDispatchToProps = (dispatch, props) => ({
+    getStateDisplay() {
+        dispatch(getStateDisplayMap());
+    },
     movePackageFiles(packageId) {
         dispatch(movePackageFiles(packageId));
     }
