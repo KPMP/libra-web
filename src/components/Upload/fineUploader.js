e519edb649c70f142f67d9b7f6cefab95b7aae15
dert1129 <nathancreger1111@gmail.com>
2026-08-19 13:25:59 -0400

    save current work

---
+++ src/components/Upload/fineUploader.js
@@ -0,0 +1,32 @@
+import FineUploaderTraditional from 'fine-uploader-wrappers';
+
+const acceptedExtensions = [
+	"yaml",
+    "yml"
+]
+
+export const getUploader = (limit = 0) =>
+{
+	return new FineUploaderTraditional ({
+		options: {
+			autoUpload: false,
+			disableCancelForFormUploads: true,
+			chunking: {
+				enabled: true
+			},
+			deleteFile: {
+				enabled: false,
+			},
+			retry: {
+				enableAuto: false
+			},
+			resume: {
+				enabled: false
+			},
+			validation: {
+				itemLimit: limit,
+				allowedExtensions: acceptedExtensions
+			}
+		}
+	});
+}
